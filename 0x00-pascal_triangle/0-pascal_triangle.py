8accbee6a358b36a6e7fbd8928547bd669d36b84
Anas8e <94750646+Anas8e@users.noreply.github.com>
2024-03-08 02:11:13 +0100

    hopefully last update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,24 @@
+#!/usr/bin/python3
+"""A script to determine pascal's triangle for any number"""
+
+
 def pascal_triangle(n):
-        # Initialize the Pascal's triangle list
-            triangle = []
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = []
 
-                # Base case: return an empty list if n is less than or equal to 0
-                    if n <= 0:
-                                return triangle
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
 
-                                # Iterate through each row of the triangle
-                                    for i in range(n):
-                                                # Initialize a new row list
-                                                        row = []
-
-                                                                # Calculate values for the current row
-                                                                        for j in range(i + 1):
-                                                                                        # If it's the first or last element of the row, append 1
-                                                                                                    if j == 0 or j == i:
-                                                                                                                        row.append(1)
-                                                                                                                                    # Otherwise, calculate the value based on the previous row
-                                                                                                                                                else:
-                                                                                                                                                                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-
-                                                                                                                                                                            # Append the row to the triangle list
-                                                                                                                                                                                    triangle.append(row)
-
-                                                                                                                                                                                        return triangle
-
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
